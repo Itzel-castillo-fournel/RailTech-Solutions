c4abe2eb0d7f27cb565db6a48f1ae03192246d8b
Dylan <dylanog1@gmail.com>
2025-02-12 10:30:41 +0100

    modification du path pour le .env, ajout d'une requête check_login

--- RailTechRH/src/main/java/fr/irontrail/railtechrh/dao/UtilisateurDAO.java
+++ RailTechRH/src/main/java/fr/irontrail/railtechrh/dao/UtilisateurDAO.java
@@ -5,4 +5,5 @@ public class UtilisateurDAO {
     private static final String GET_USER = "SELECT id, nom, prenom, email, role FROM Utilisateur WHERE id = ?";
     private static final String UPDATE_USER = "UPDATE Utilisateur SET nom = ?, prenom = ?, email = ? WHERE id = ?";
     private static final String DELETE_USER = "DELETE FROM Utilisateur WHERE id = ?";
+    private static final String CHECK_LOGIN = "SELECT id, nom, prenom, role FROM Utilisateur WHERE email = ? AND mdp = ?";
 }
