4bbfaca85d8fb98466e58f39de2262b795e71e18
malika95290 <malika.amranimaam@gmail.com>
2025-02-18 13:41:43 +0100

    ajout des heures effectuées par le conducteur sur sa page profil (dao,controller et fxml)

--- RailTechRH/src/main/java/fr/irontrail/railtechrh/controller/UserProfilController.java
+++ RailTechRH/src/main/java/fr/irontrail/railtechrh/controller/UserProfilController.java
@@ -13,7 +13,8 @@ public class UserProfilController {
     @FXML private Label roleLabel;
     @FXML private Label trajetsLabel; // Nouveau label pour afficher le nombre de trajets
     @FXML private AnchorPane trajetsAnchorPane;
-
+    @FXML private Label heuresLabel; // Nouveau label pour afficher le nombre d'heures
+    @FXML private AnchorPane heuresAnchorPane;
     private UtilisateurDAO utilisateurDAO = new UtilisateurDAO();
 
     public void setUser(UtilisateurModel utilisateur) {
@@ -25,7 +26,12 @@ public class UserProfilController {
         if (utilisateur.getRole() == Role.CONDUCTEUR) {
             int nombreTrajets = utilisateurDAO.getNombreTrajetsCeMois(utilisateur.getId());
             trajetsLabel.setText(String.valueOf(nombreTrajets));
-            trajetsAnchorPane.setVisible(true); // Rendre l'AnchorPane visible
+            trajetsLabel.setVisible(true);
+            trajetsAnchorPane.setVisible(true);
+            // Afficher le nombre d'heures travaillées pour tous les utilisateurs
+            int nombreHeures = utilisateurDAO.getNombreHeuresTravailleesCeMois(utilisateur.getId());
+            heuresLabel.setText(String.valueOf(nombreHeures));
+            heuresAnchorPane.setVisible(true);
         } else {
             trajetsAnchorPane.setVisible(false); // Cacher l'AnchorPane pour les autres rôles
         }
