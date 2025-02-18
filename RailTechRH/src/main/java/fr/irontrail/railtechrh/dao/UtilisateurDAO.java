fc0fda3b795daff6d998ce1e6f075b1785a5562d
Dylan <dylanog1@gmail.com>
2025-02-18 11:23:45 +0100

    résolution problème .env + suppression commentaire + mise à jour javafx en 23.0.1

--- RailTechRH/src/main/java/fr/irontrail/railtechrh/dao/UtilisateurDAO.java
+++ RailTechRH/src/main/java/fr/irontrail/railtechrh/dao/UtilisateurDAO.java
@@ -1,12 +1,5 @@
 package fr.irontrail.railtechrh.dao;
 
-//public class UtilisateurDAO {
-//    private static final String INSERT_USER = "INSERT INTO Utilisateur (nom, prenom, email, mdp, role) VALUES (?, ?, ?, ?, ?)";
-//    private static final String GET_USER = "SELECT id, nom, prenom, email, role FROM Utilisateur WHERE id = ?";
-//    private static final String UPDATE_USER = "UPDATE Utilisateur SET nom = ?, prenom = ?, email = ? WHERE id = ?";
-//    private static final String DELETE_USER = "DELETE FROM Utilisateur WHERE id = ?";
-//}
-
 import fr.irontrail.railtechrh.model.UtilisateurModel;
 import fr.irontrail.railtechrh.model.enums.Role;
 
@@ -16,6 +9,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UtilisateurDAO {
+    private static final String INSERT_USER = "INSERT INTO Utilisateur (nom, prenom, email, mdp, role) VALUES (?, ?, ?, ?, ?)";
+    private static final String GET_USER = "SELECT id, nom, prenom, email, role FROM Utilisateur WHERE id = ?";
+    private static final String UPDATE_USER = "UPDATE Utilisateur SET nom = ?, prenom = ?, email = ? WHERE id = ?";
+    private static final String DELETE_USER = "DELETE FROM Utilisateur WHERE id = ?";
+    private static final String CHECK_LOGIN = "SELECT id, nom, prenom, role FROM Utilisateur WHERE email = ? AND mdp = ?";
+
     public UtilisateurModel findByEmail(String email) {
         // Connexion à la base de données
         try (Connection connection = DatabaseConnection.getConnection()) {
