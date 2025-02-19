2fd4d5a2fd4ba2d9ceb68869b4e6305459154c9e
Itzel-castillo-fournel <libitzel.castillo@gmail.com>
2025-02-19 16:05:04 +0100

    IncidentDAO -> createIncident

---
+++ RailTechRH/src/main/java/fr/irontrail/railtechrh/dao/IncidentDAO.java
@@ -0,0 +1,45 @@
+package fr.irontrail.railtechrh.dao;
+
+import fr.irontrail.railtechrh.model.IncidentModel;
+import fr.irontrail.railtechrh.model.TrainModel;
+import fr.irontrail.railtechrh.model.enums.Gravite;
+import fr.irontrail.railtechrh.model.enums.TypeIncident;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class IncidentDAO {
+    private static final String INSERT_INCIDENT = "INSERT INTO Incident (description, typeIncident, gravite, trainImmat VALUES (?,?,?,?)";
+    private static final String GET_INCIDENT = "SELECT id, description, typeIncident, gravite, trainImmat FROM Incident WHERE id = ?";
+    private static final String UPDATE_INCIDENT = "UPDATE Incident SET descritpion = ?, typeIncident = ?, gravite = ?, trainImmat = ? WHERE id = ?";
+    private static final String DELETE_INCIDENT = "DELETE FROM Incident WHERE id = ?";
+
+    public IncidentModel createIncident(String description, TypeIncident typeIncident, Gravite gravite, TrainModel trainImmat) throws SQLException {
+        String [] generatedColumns = {"id"};
+
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(INSERT_INCIDENT);
+            statement.setString(1, description);
+            statement.setString(2, typeIncident.toString());
+            statement.setString(3, gravite.toString());
+            statement.setString(4, trainImmat.getImmatriculation());
+
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int id = generatedKeys.getInt(1);
+                        System.out.println("Incident created successfully");
+                        return new IncidentModel(id, description, typeIncident, gravite, trainImmat);
+
+                    }
+                }
+            }
+        }catch (SQLException e) {
+            System.out.println("Error inserting incident : " + e.getMessage());
+        }
+        return null;
+    }
+}
--- RailTechRH/src/main/java/fr/irontrail/railtechrh/dao/IncidentModel.java
+++
@@ -1,5 +0,0 @@
-package fr.irontrail.railtechrh.dao;
-
-public class IncidentModel {
-    String sql = "INSERT INTO incident (description, typeIncident, gravite, trainImmat VALUES (?,?,?,?)";
-}
